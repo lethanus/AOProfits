10de45f67c6ef8e92ffb0ad321832a463d2bc966
Emil <emil.drozdowski@LAPTOP-CR7CEFSM>
2019-04-02 17:13:01 +0200

    Fake Price provider

--- ConstructionYard/ConstructionYard/Class1.cs
+++ ConstructionYard/ConstructionYard/Class1.cs
@@ -11,52 +11,57 @@ namespace ConstructionYard
     public class Class1
     {
 
-        [TestCase(10, new int[] { 58, 71, 92 }, 580)]
-        public void CalcMinValue(int itemCount, int[] itemPrices, int expectedMinValue)
+        [TestCase(10, "Rock2", 580)]
+        public void CalcMinValue(int itemCount, string productCode, int expectedMinValue)
         {
-            int minValue = MinValue(itemCount, itemPrices);
+            int minValue = MinValue(itemCount, GetPrices(productCode));
 
             Assert.AreEqual(expectedMinValue, minValue);
         }
 
-        [TestCase(10, new int[] { 58, 71, 92 }, 1160)]
-        public void CalcMinValueForTheStoneBlock2(int itemCount, int[] itemPrices, int expectedMinValue)
+        [TestCase(10, "Rock2", 1160)]
+        public void CalcMinValueForTheStoneBlock2(int itemCount, string productCode, int expectedMinValue)
         {
 
-            int minValue = MinValue(itemCount, itemPrices);
+            int minValue = MinValue(itemCount, GetPrices(productCode));
 
             int stoneBlockPV = minValue * 2;
 
             Assert.AreEqual(expectedMinValue, stoneBlockPV);
         }
 
-        [TestCase(10, new int[] { 58, 71, 92 }, 920)]
-        public void CalcMaxValue(int itemCount, int[] itemPrices, int expectedMaxValue)
+        [TestCase(10, "Rock2", 920)]
+        public void CalcMaxValue(int itemCount, string productCode, int expectedMaxValue)
         {
-            int minValue = MaxValue(itemCount, itemPrices);
+            int minValue = MaxValue(itemCount, GetPrices(productCode));
 
             Assert.AreEqual(expectedMaxValue, minValue);
         }
 
-        [TestCase(10, new int[] { 58, 71, 92 }, 1840)]
-        public void CalcMaxValueForTheStoneBlock2(int itemCount, int[] itemPrices, int expectedMaxValue)
+        [TestCase(10, "Rock2", 1840)]
+        public void CalcMaxValueForTheStoneBlock2(int itemCount, string productCode, int expectedMaxValue)
         {
 
-            int minValue = MaxValue(itemCount, itemPrices);
+            int minValue = MaxValue(itemCount, GetPrices(productCode));
 
             int stoneBlockPV = minValue * 2;
 
             Assert.AreEqual(expectedMaxValue, stoneBlockPV);
         }
 
-        private int MinValue(int count, int[] prices)
+        private int MinValue(int count, IEnumerable<int> prices)
         {
             return count * prices.Min();
         }
 
-        private int MaxValue(int count, int[] prices)
+        private int MaxValue(int count, IEnumerable<int> prices)
         {
             return count * prices.Max();
+        }
+
+        private IEnumerable<int> GetPrices(string itemName)
+        {
+            return new int[] { 58, 71, 92 };
         }
     }
 }
